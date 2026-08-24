e090c1fdce21953d193f68ba566dda727b460a1d
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-24 15:09:18 -0300

    chore(all): inicializacion de proyecto

---
+++ src/main/java/com/chacuio/issueflowapi/IssueflowapiApplication.java
@@ -0,0 +1,13 @@
+package com.chacuio.issueflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueflowapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chacuio/issueflowapi/IssueflowapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chacuio.issueflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueflowapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

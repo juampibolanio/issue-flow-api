13ba82b5801c2e45df2c20deefb167a0fb91c96c
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-24 20:27:50 -0300

    fix(bd): corregir campos de tablas ticket y usuario

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -2,24 +2,29 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
-    password VARCHAR(256) NOT NULL,
-    CONSTRAINT role CHECK ( role IN ('EMPLOYEE', 'DEVELOPER') ),
+    password VARCHAR NOT NULL,
+    role VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+
+    CONSTRAINT role CHECK ( role IN ('EMPLOYEE', 'DEVELOPER') ),
 );
 
 CREATE TABLE tickets (
     id UUID PRIMARY KEY,
     title VARCHAR(256) NOT NULL,
     description VARCHAR,
-    CONSTRAINT state CHECK ( state IN ('PENDING', 'IN_PROGRESS', 'CLOSED') ),
-    CONSTRAINT priority CHECK ( priority IN ('URGENT', 'HIGH', 'MEDIUM', 'LOW')),
+    state VARCHAR(30) NOT NULL,
+    priority VARCHAR(30) NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
-    assigned UUID,
-    reporter UUID,
+    assigned_id UUID,
+    reporter_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
 
     CONSTRAINT fk_assigned_user FOREIGN KEY (assigned) REFERENCES users(id),
-    CONSTRAINT fk_reporter_user FOREIGN KEY (reporter) REFERENCES users(id)
+    CONSTRAINT fk_reporter_user FOREIGN KEY (reporter) REFERENCES users(id),
+
+    CONSTRAINT state CHECK ( state IN ('PENDING', 'IN_PROGRESS', 'CLOSED') ),
+    CONSTRAINT priority CHECK ( priority IN ('URGENT', 'HIGH', 'MEDIUM', 'LOW')),
 );

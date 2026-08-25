f045e25e736bf9ec38ab0b9f7dc7baade043c0f7
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-25 13:40:23 -0300

    fix(db): corregir errores de sintaxis y nombres de restricciones

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -7,14 +7,14 @@ CREATE TABLE users (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
 
-    CONSTRAINT role CHECK ( role IN ('EMPLOYEE', 'DEVELOPER') ),
+    CONSTRAINT chk_users_role CHECK ( role IN ('EMPLOYEE', 'DEVELOPER') )
 );
 
 CREATE TABLE tickets (
     id UUID PRIMARY KEY,
     title VARCHAR(256) NOT NULL,
     description VARCHAR,
-    state VARCHAR(30) NOT NULL,
+    state VARCHAR(30) NOT NULL DEFAULT 'PENDING',
     priority VARCHAR(30) NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
     assigned_id UUID,
@@ -22,9 +22,9 @@ CREATE TABLE tickets (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
 
-    CONSTRAINT fk_assigned_user FOREIGN KEY (assigned) REFERENCES users(id),
-    CONSTRAINT fk_reporter_user FOREIGN KEY (reporter) REFERENCES users(id),
+    CONSTRAINT fk_assigned_user FOREIGN KEY (assigned_id) REFERENCES users(id),
+    CONSTRAINT fk_reporter_user FOREIGN KEY (reporter_id) REFERENCES users(id),
 
-    CONSTRAINT state CHECK ( state IN ('PENDING', 'IN_PROGRESS', 'CLOSED') ),
-    CONSTRAINT priority CHECK ( priority IN ('URGENT', 'HIGH', 'MEDIUM', 'LOW')),
+    CONSTRAINT chk_tickets_state CHECK ( state IN ('PENDING', 'IN_PROGRESS', 'CLOSED') ),
+    CONSTRAINT chk_tickets_priority CHECK ( priority IN ('URGENT', 'HIGH', 'MEDIUM', 'LOW'))
 );

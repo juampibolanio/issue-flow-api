585e830762a86620e954b2e21ddc4a4fc082a828
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-26 12:22:25 -0300

    feat(users): agregar mapper de users

---
+++ src/main/java/com/chacuio/issueflowapi/users/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.chacuio.issueflowapi.users.mapper;
+
+import com.chacuio.issueflowapi.users.dto.UserDTO;
+import com.chacuio.issueflowapi.users.dto.UserRequestDTO;
+import com.chacuio.issueflowapi.users.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserDTO toDto(User user) {
+        return new UserDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole(),
+                user.isActive(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
+    public User toEntity(UserRequestDTO dto) {
+        return User.builder()
+                .name(dto.name())
+                .email(dto.email())
+                .password(dto.password())
+                .role(dto.role())
+                .build();
+    }
+}

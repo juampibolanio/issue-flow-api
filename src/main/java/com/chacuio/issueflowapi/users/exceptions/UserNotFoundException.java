aa6d295b114a2c4bb1f23a9d93fa85351ca78316
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-26 16:22:28 -0300

    feat(users): agregar constructores con valores por defecto para excepcion de usuario no encontrado

--- src/main/java/com/chacuio/issueflowapi/users/exceptions/UserNotFoundException.java
+++ src/main/java/com/chacuio/issueflowapi/users/exceptions/UserNotFoundException.java
@@ -1,7 +1,17 @@
 package com.chacuio.issueflowapi.users.exceptions;
 
+import java.util.UUID;
+
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        this("User not found");
+    }
+
     public UserNotFoundException(String message) {
         super(message);
+    }
+
+    public UserNotFoundException(UUID id) {
+        this("User with ID: " + id + " not found");
     }
 }

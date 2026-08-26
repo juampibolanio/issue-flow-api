b7fa2214cd8f8c134aa9f23e2498980ff9cbcc1d
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-26 12:24:26 -0300

    fix(tickets): eliminar comentarios innecesarios

--- src/main/java/com/chacuio/issueflowapi/tickets/dto/AssignTicketRequestDTO.java
+++ src/main/java/com/chacuio/issueflowapi/tickets/dto/AssignTicketRequestDTO.java
@@ -6,7 +6,4 @@ public record AssignTicketRequestDTO(
         UUID ticketId,
         UUID assignedId
         // el id del usuario que hizo la asignación va en el header, se lee de ahí
-
-        //Seguir con esto: Revisar bien cada uno de los dto y mappers de user y ticket.
-        // luego queda hacer el servicio y sus implementaciones + toda la logica de negocio
 ) { }

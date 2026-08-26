08f1c8a59d22b71a89ba732999b0b39631bd7581
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-26 16:22:56 -0300

    feat(users): agregar método toSummary para mapear datos de usuario a un resumen

--- src/main/java/com/chacuio/issueflowapi/users/mapper/UserMapper.java
+++ src/main/java/com/chacuio/issueflowapi/users/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.chacuio.issueflowapi.users.mapper;
 
 import com.chacuio.issueflowapi.users.dto.UserDTO;
 import com.chacuio.issueflowapi.users.dto.UserRequestDTO;
+import com.chacuio.issueflowapi.users.dto.UserSummaryDTO;
 import com.chacuio.issueflowapi.users.model.User;
 import org.springframework.stereotype.Component;
 
@@ -26,5 +27,16 @@ public class UserMapper {
                 .password(dto.password()) // the password here should be hashed. In the future, implement a password encoder
                 .role(dto.role())
                 .build();
+    }
+
+    public UserSummaryDTO toSummary(User user) {
+        if (user == null) {
+            return null;
+        }
+        return new UserSummaryDTO(
+                user.getId(),
+                user.getName(),
+                user.isActive()
+        );
     }
 }
